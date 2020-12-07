4e78e2a73fb9869af63ebf3b75b5a8e067ac5828
socratone <gim2origin@gmail.com>
2020-12-08 05:01:22 +0900

    클래스 constructor 축약 문법 추가

--- src/index.ts
+++ src/index.ts
@@ -12,24 +12,20 @@ interface Human {
 }
 
 function sayHi(person: Human): string {
-  return `안녕 나는 ${person.age}살의 ${person.name}이라고 해.`
+  return `안녕 나는 ${person.age}살의 ${person.name}이라고 해.`;
 }
 
 console.log('sayHi(Human):', sayHi({ name: 'socratone', age: 24 }));
 
 // 클래스
 class Car {
-  public position: number;
-  private engine: string; // private은 car 인스턴스로 접근할 수 없다.
-
-  constructor(position: number) {
-    this.position = position;
-    this.engine = 'super';
+  constructor(public position: number, private engine: string) {
+    // public이나 private을 붙여주면 this.position = position;을 생략할 수 있다.
   }
 }
 
-const car = new Car(5);
-console.log('car.position:', car.position); 
+const car = new Car(5, 'super');
+console.log('car.position:', car.position); // public만 접근할 수 있다.
 
 // enum
 enum Color { Red, Green, Blue = 'blue' };
